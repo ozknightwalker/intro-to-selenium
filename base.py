1b1dcdfc6f0ba7b11b77b7ea406a7702a94d981f
Jc <ozknightwalker62@gmail.com>
2017-11-03 13:30:27 +0800

    remove setUpTestData since it is not in python unittest

--- base.py
+++ base.py
@@ -5,15 +5,17 @@ import unittest
 
 class BaseTestCase(unittest.TestCase):
 
-    @classmethod
-    def setUpTestData(self):
-        pass
-
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
+
+    def test_pass(self):
+        self.assertTrue(True)
+
+    def test_fail(self):
+        self.assertTrue(False)
 
 
 if __name__ == '__main__':
