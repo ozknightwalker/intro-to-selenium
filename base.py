0aa373cd156a8947188bc050681dfb64cf1dcf6d
Jc Din <jcdin@channelfix.com>
2018-03-05 09:52:20 +0800

    update to only open and close the browser once

--- base.py
+++ base.py
@@ -7,11 +7,25 @@ from selenium import webdriver
 
 class BaseTestCase(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        """ setUpClass is called before all test runs """
+        super(BaseTestCase, cls).setUpClass()
+        cls.driver = webdriver.Firefox()
+
+    @classmethod
+    def tearDownClass(cls):
+        """ tearDownClass is called when all test are done """
+        cls.driver.close()
+        super(BaseTestCase, cls).tearDownClass()
+
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        """ setUp runs when every start of every test """
+        pass
 
     def tearDown(self):
-        self.driver.close()
+        """ tearDown run at the end of every test """
+        pass
 
     def test_interact_form(self):
         self.driver.get(
@@ -22,8 +36,7 @@ class BaseTestCase(unittest.TestCase):
         # field.send_keys(Keys.ENTER)
 
     def test_interact_options(self):
-        self.driver.get(
-            'https://channelfix.com/#ranking')
+        self.driver.get('https://channelfix.com/#ranking')
         # iterate all options
         select = self.driver.find_element_by_class_name('categories')
         options = select.find_elements_by_tag_name('option')
